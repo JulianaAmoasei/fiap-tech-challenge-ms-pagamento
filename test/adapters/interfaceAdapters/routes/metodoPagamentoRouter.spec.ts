3a0318a8e82240a6629508a1f1d4292b258f630f
JulianaAmoasei <late.parrot@gmail.com>
2024-01-27 05:15:20 -0300

    test: :white_check_mark: add: testes de rota

---
+++ test/adapters/interfaceAdapters/routes/metodoPagamentoRouter.spec.ts
@@ -0,0 +1,67 @@
+import express from "express";
+import supertest from "supertest";
+import { v4 as uuidv4 } from "uuid";
+
+import MetodoPagamentoController from "../../../../src/adapters/interfaceAdapters/controllers/metodoPagamentoController";
+import routes from "../../../../src/adapters/interfaceAdapters/routes";
+
+const app = express();
+app.use(express.json());
+routes(app);
+const server = app.listen(4000);
+
+afterAll(() => {
+  server.close()
+})
+
+const createdAt = new Date();
+
+const metodoPagamentoMock = {
+  _id: uuidv4(),
+  nome: "QR Code",
+  ativo: true,
+  createdAt,
+  deletedAt: null,
+  updatedAt: null,
+};
+
+const listaMetodosPagamentoMock = [
+  {
+    _id: uuidv4(),
+    nome: "QR Code",
+    ativo: true,
+    createdAt,
+    deletedAt: null,
+    updatedAt: null,
+  },
+];
+
+describe("GET em /api/metodo-pagamento", () => {
+  it("Deve listar os métodos de pagamento", async () => {
+    MetodoPagamentoController.listaMetodosPagamento = jest
+      .fn()
+      .mockResolvedValue(listaMetodosPagamentoMock);
+
+    await supertest(server)
+      .get("/api/metodo-pagamento")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.message[0].nome).toBe("QR Code");
+      });
+  });
+});
+
+describe("GET em /api/metodo-pagamento/default", () => {
+  it("Deve retornar o método de pagamento padrão", async () => {
+    MetodoPagamentoController.retornaMetodoPagamentoPadraoId = jest
+      .fn()
+      .mockResolvedValue(metodoPagamentoMock);
+
+    await supertest(server)
+      .get("/api/metodo-pagamento/default")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.message.nome).toBe("QR Code");
+      });
+  });
+});
